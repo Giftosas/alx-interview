dd09dfc04a1819b2ff3725bd531d3adf4cf114bd
Giftosas <osariemenchantelle6@gmail.com>
2023-12-09 11:17:01 -0800

    Task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-""" Lockboxes """
+"""Defines a function that determines if a box containing a list
+   of lists can be opened using keys stored in the lists
+"""
 
 
 def canUnlockAll(boxes):
-    """ Method that determines if all the boxes can be opened """
-    if len(boxes) == 0:
-        return False
-    keys = [0]
-    for key in keys:
-        for box in boxes[key]:
-            if box not in keys and box < len(boxes):
-                keys.append(box)
-    if len(keys) == len(boxes):
-        return True
+    """Determines if boxes can be unlocked"""
+    position = 0
+    unlocked = {}
+
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[position] = "always_unlocked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        position += 1
     return False
