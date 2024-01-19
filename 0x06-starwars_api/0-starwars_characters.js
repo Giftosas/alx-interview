f806e1f5ef476710631c9852c7d6544f8d4c423a
Giftosas <osariemenchantelle6@gmail.com>
2024-01-19 12:47:53 -0800

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,51 +1,24 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieId = process.argv[2];
+const MyArgs = process.argv.slice(2);
 
-function getMovieCharacters(movieId) {
-  const filmsUrl = 'https://swapi-api.alx-tools.com/api/films';
+const url = `https://swapi-api.hbtn.io/api/films/${MyArgs}`;
 
-  request.get(filmsUrl, (error, response, body) => {
-    if (response.statusCode === 200) {
-      const filmsData = JSON.parse(body);
-      const movieData = filmsData.results.find((film) => film.episode_id.toString() === movieId);
-
-      if (movieData) {
-        const charactersUrls = movieData.characters;
-        const characters = [];
-
-        for (const charUrl of charactersUrls) {
-          request.get(charUrl, (error, response, body) => {
-            if (response.statusCode === 200) {
-              const characterData = JSON.parse(body);
-              characters.push(characterData.name);
-              if (characters.length === charactersUrls.length) {
-                printCharacters(characters, movieData.title);
-              }
-            } else {
-              console.log(`Error: ${response.statusCode}`);
-            }
-          });
-        }
-      } else {
-        console.log(`Movie ID ${movieId} not found.`);
-      }
-    } else {
-      console.log(`Error: ${response.statusCode}`);
-    }
-  });
-}
-
-function printCharacters(characters, movieTitle) {
-  console.log(`Characters in ${movieTitle}:`);
-  for (const character of characters) {
-    console.log(character);
+request(url, async (error, response, body) => {
+  if (error) {
+    return console.log(error);
   }
-}
-
-if (movieId) {
-  getMovieCharacters(movieId);
-} else {
-  console.log('Usage: node 0-starwars_characters.js [Movie ID]');
-}
+  const res = JSON.parse(body).characters;
+  for (let i = 0; i < res.length; i++) {
+    await new Promise((resolve, reject) => {
+      request(res[i], (e, resp, bod) => {
+        if (e) {
+          reject(e);
+        }
+        console.log(JSON.parse(bod).name);
+        resolve();
+      });
+    });
+  }
+});
